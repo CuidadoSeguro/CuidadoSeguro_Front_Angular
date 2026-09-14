ff7abd716301dacd6cf1de27ec83ac9be936faff
Carlos271085 <car.bernal@duocuc.cl>
2026-09-14 19:47:51 -0300

    chore: configurar URLs de producción2

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     clientId: '3912eb25-8b20-4725-9b9d-18a99c419ead',
     tenantId: '95cd823a-c239-4ece-b6ea-932724964971',
     //redirectUri: 'http://localhost:4200/login',
-    redirectUri: 'https://cuidadoseguro.github.io/CuidadoSeguro_Front_Angular/login'
+    redirectUri: 'https://cuidadoseguro.github.io/CuidadoSeguro_Front_Angular/login',
     scopes: [
       'api://3912eb25-8b20-4725-9b9d-18a99c419ead/access_as_user'
     ],
